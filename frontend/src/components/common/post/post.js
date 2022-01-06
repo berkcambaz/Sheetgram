7253d7ff2e2d26e037f561b07d03d86c26315958
Berk Cambaz <berkcambaz12321@gmail.com>
2022-01-06 21:48:00 +0300

    Add like and bookmark

--- frontend/src/components/common/post/post.js
+++ frontend/src/components/common/post/post.js
@@ -1,6 +1,38 @@
 import { lucid } from "../../../libs/lucid";
 
+import { Component_Icon_Like } from "../../icons/like";
+import { Component_Icon_Bookmark } from "../../icons/bookmark";
+
 export const Component_Post = lucid.component({
+  attributes: function () {
+    return {
+      user: {},
+      post: {
+        liked: false,
+        bookmarked: false
+      }
+    }
+  },
+  methods: {
+    getLikeClass: function () {
+      return this.attributes.post.liked ? "post__icon enabled" : "post__icon";
+    },
+    getBookmarkClass: function () {
+      return this.attributes.post.bookmarked ? "post__icon enabled" : "post__icon";
+    },
+    gotoProfile: function () {
+
+    },
+    like: function () {
+      this.attributes.post.liked = !this.attributes.post.liked;
+      lucid.instance(Component_Icon_Like, this.key).attribute("class", this.methods.getLikeClass());
+      this.setState();
+    },
+    bookmark: function () {
+      lucid.instance(Component_Icon_Bookmark, this.key).attribute("class", this.methods.getBookmarkClass());
+      this.setState();
+    }
+  },
   render: function () {
     return `
       <div class="post">
@@ -12,10 +44,16 @@ export const Component_Post = lucid.component({
           <span class="post__date">12h</span>
         </div>
         <div class="post__content">hello</div>
-        <div class="post__bottom">
+        <div class="post__bottom" lucid-ref="bottom">
           <span class="post__count">123</span>
         </div>
       </div>
     `;
+  },
+  hooks: {
+    connected: function () {
+      lucid.render(this.refs["bottom"], Component_Icon_Like, this.key, { class: this.methods.getLikeClass(), onclick: this.methods.like });
+      lucid.render(this.refs["bottom"], Component_Icon_Bookmark, this.key, { class: this.methods.getBookmarkClass(), onclick: this.methods.bookmark });
+    }
   }
 });
