e8342e22367fcb8e953464d722bf00b7d7dd372b
Berk Cambaz <berkcambaz12321@gmail.com>
2022-01-07 20:45:38 +0300

    Fix script typo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
+const path = require('path')
+
 const express = require("express");
 const app = express();
 
-app.use(express.static("../../frontend/public"));
+//app.use(express.static("../../frontend/public"));
+console.log("test");
+app.use('/static', express.static(path.join(__dirname, '../../frontend/public')))
 
 app.listen(process.env.PORT || 80);
