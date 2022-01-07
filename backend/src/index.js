be9adf87073244a7b7a50fd5c6129c7147ef2fea
Berk Cambaz <berkcambaz12321@gmail.com>
2022-01-07 21:58:17 +0300

    Add supabase client

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
-const path = require('path')
+const path = require("path")
 
 const express = require("express");
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, '../../frontend/public')));
+app.use("/", express.static(path.join(__dirname, "../../frontend/public")));
+
+app.post("/api", function (req, res) {
+
+});
 
 app.listen(process.env.PORT || 80);
+
+console.log("Server has started...");
