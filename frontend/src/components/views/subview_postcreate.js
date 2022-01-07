08a7c9e1b1af29c12b090b91fc7bbbd455b9119d
Berk Cambaz <berkcambaz12321@gmail.com>
2022-01-07 14:24:29 +0300

    Improve postcreate component and move scrollbar from border of the screen to main view

--- frontend/src/components/views/subview_postcreate.js
+++ frontend/src/components/views/subview_postcreate.js
@@ -1,13 +1,32 @@
 import { lucid } from "../../libs/lucid";
 
+import { detectClick } from "../../core/utility";
+
 export const Component_SubView_PostCreate = lucid.component({
+  state: function () { return { removeListener: undefined } },
   render: function () {
     return `
-      <div class="post-create effect--blur">
-        <div class="post-create__container">
+      <div>
+        <div class="effect--blur"></div>
+        <div class="post-create" lucid-ref="post-create">
           hello world
         </div>
       </div>
     `;
+  },
+  hooks: {
+    connected: function () {
+      setTimeout(() => {
+        this.state.removeListener = detectClick(this.refs["post-create"],
+          (ev) => {
+
+          },
+          (ev) => {
+            this.state.removeListener();
+            lucid.remove(Component_SubView_PostCreate, this.key);
+          }
+        );
+      }, 1);
+    }
   }
 });
--- frontend/src/core/utility.js
+++ frontend/src/core/utility.js
@@ -7,3 +7,25 @@ export function clampNumber(number) {
   if (number > 999) return Math.floor(number / 1000).toString() + "k";
   return number;
 }
+
+/**
+ * 
+ * @param {HTMLElement} dom 
+ * @param {(ev: MouseEvent | TouchEvent) => boolean} inside 
+ * @param {(ev: MouseEvent | TouchEvent) => boolean} outside 
+ * @returns 
+ */
+export function detectClick(dom, inside, outside) {
+  const func = function (ev) {
+    if (dom.contains(ev.target)) return inside();
+    else return outside();
+  }
+
+  document.addEventListener("click", func);
+  document.addEventListener("touchstart", func);
+
+  return () => {
+    document.removeEventListener("click", func);
+    document.removeEventListener("touchstart", func);
+  };
+}
