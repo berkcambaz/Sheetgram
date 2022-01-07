01408c02f7b72315374cc079c7fa3363658f0bbc
Berk Cambaz <berkcambaz12321@gmail.com>
2022-01-07 21:00:38 +0300

    Remove nodemon & add supervisor, fix express static routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,6 @@ const path = require('path')
 const express = require("express");
 const app = express();
 
-//app.use(express.static("../../frontend/public"));
-console.log("test");
-app.use('/static', express.static(path.join(__dirname, '../../frontend/public')))
+app.use('/', express.static(path.join(__dirname, '../../frontend/public')));
 
 app.listen(process.env.PORT || 80);
