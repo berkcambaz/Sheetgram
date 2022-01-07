0e72c8a88aaae713438d4b6e31789b54e9bae019
Berk Cambaz <berkcambaz12321@gmail.com>
2022-01-07 16:53:09 +0300

    Implement login & signup views

--- frontend/src/components/views/view_login.js
+++ frontend/src/components/views/view_login.js
@@ -1,12 +1,23 @@
 import { lucid } from "../../libs/lucid";
+import { superpage } from "../../libs/superpage";
 
 export const Component_View_Login = lucid.component({
-  render: function () {
-    return `<div></div>`;
-  },
-  hooks: {
-    connected: function () {
-
+  methods: {
+    login: function () {
+      // TODO: Implement
+    },
+    gotoSignup: function () {
+      superpage.to("/signup");
     }
+  },
+  render: function () {
+    return `
+      <div class="login">
+        <input class="login input" type="text" placeholder="Username...">
+        <input class="login input" type="password" placeholder="Password...">
+        <button class="login button" onclick="{{methods.login}}">Log in</button>
+        <div class="login button--text" onclick="{{methods.gotoSignup}}">I don't have an account</div>
+      </div>
+    `;
   }
 });
--- frontend/src/components/views/view_signup.js
+++ frontend/src/components/views/view_signup.js
@@ -1,12 +1,25 @@
 import { lucid } from "../../libs/lucid";
+import { superpage } from "../../libs/superpage";
 
 export const Component_View_Signup = lucid.component({
-  render: function () {
-    return `<div></div>`;
-  },
-  hooks: {
-    connected: function () {
-
+  methods: {
+    signup: function () {
+      // TODO: Implement
+    },
+    gotoLogin: function () {
+      superpage.to("/login");
     }
+  },
+  render: function () {
+    return `
+      <div class="signup">
+        <input class="signup input" type="text" placeholder="Username...">
+        <input class="signup input" type="mail" placeholder="E-mail...">
+        <input class="signup input" type="password" placeholder="Password...">
+        <input class="signup input" type="password" placeholder="Confirm password...">
+        <button class="signup button" onclick="{{methods.signup}}">Sign up</button>
+        <div class="signup button--text" onclick="{{methods.gotoLogin}}">I already have an account</div>
+      </div>
+    `;
   }
 });
